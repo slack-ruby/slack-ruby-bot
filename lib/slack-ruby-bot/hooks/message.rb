fa17932abf5ad0c5d25c652f4a46cec1d93a99d4
dB <dblock@dblock.org>
2015-07-02 15:07:02 -0400

    Fix: remove hack from mathbot.

--- lib/slack-ruby-bot/hooks/message.rb
+++ lib/slack-ruby-bot/hooks/message.rb
@@ -15,12 +15,7 @@ module SlackRubyBot
 
       def parse_command(text)
         return unless text
-        text = '= ' + text[1..text.length] if text[0] == '='
         parts = text.split.reject(&:blank?)
-        if parts && parts[0] == '='
-          parts[0] = SlackRubyBot.config.user
-          parts.insert 1, 'calculate'
-        end
         [parts.first.downcase, parts[1].try(:downcase), parts[2..parts.length]] if parts && parts.any?
       end
 
