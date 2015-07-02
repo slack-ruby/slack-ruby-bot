a15d7d4b801c76ec3da6383cb85b4ae1b3c1d88d
dB <dblock@dblock.org>
2015-07-02 14:32:38 -0400

    Created shared behavior for slack ruby bot.

---
+++ lib/slack-ruby-bot/rspec/support/slack-ruby-bot/it_behaves_like_a_slack_bot.rb
@@ -0,0 +1,27 @@
+shared_examples 'a slack ruby bot' do
+  context 'not configured' do
+    before do
+      @slack_api_token = ENV.delete('SLACK_API_TOKEN')
+    end
+    after do
+      ENV['SLACK_API_TOKEN'] = @slack_api_token
+    end
+    it 'requires SLACK_API_TOKEN' do
+      expect { subject }.to raise_error RuntimeError, "Missing ENV['SLACK_API_TOKEN']."
+    end
+  end
+  context 'configured', vcr: { cassette_name: 'auth_test' } do
+    context 'run' do
+      before do
+        subject.send(:auth!)
+      end
+      it 'succeeds auth' do
+        expect(subject.config.url).to eq 'https://rubybot.slack.com/'
+        expect(subject.config.team).to eq 'team'
+        expect(subject.config.user).to eq 'user'
+        expect(subject.config.team_id).to eq 'TDEADBEEF'
+        expect(subject.config.user_id).to eq 'UBAADFOOD'
+      end
+    end
+  end
+end
