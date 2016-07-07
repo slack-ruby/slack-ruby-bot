9bda0893cca747ee91ad64e183aad06397d9ee85
Ryan Mulligan <rmulligan@yottamark.com>
2016-07-06 22:21:00 -0700

    Removed global variable for logger

--- lib/slack-ruby-bot/support/loggable.rb
+++ lib/slack-ruby-bot/support/loggable.rb
@@ -1,17 +1,23 @@
 module SlackRubyBot
   module Loggable
+    mattr_accessor :logger
+
     def self.included(base)
       base.send :include, LoggingMethods
       base.extend(LoggingMethods)
+    end
+
+    def self.logger
+      @logger ||= begin
+                    $stdout.sync = true
+                    Logger.new(STDOUT)
+                  end
     end
   end
 
   module LoggingMethods
     def logger
-      $logger ||= begin
-                    $stdout.sync = true
-                    Logger.new(STDOUT)
-                  end
+      Loggable.logger
     end
   end
 end
