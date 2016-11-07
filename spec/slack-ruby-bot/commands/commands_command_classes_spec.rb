b1c732701bab163e5944557dc412d6da5623761b
gregg <gconklin@gmail.com>
2016-11-07 20:20:16 +0000

    Issue #75 maintain an ordered list of command classes
    Commands order #75 notes
    
    maintain an array of registered command classes

---
+++ spec/slack-ruby-bot/commands/commands_command_classes_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe SlackRubyBot::Commands::Base do
+  let! :command1 do
+    Class.new(SlackRubyBot::Commands::Base) do
+      command 'command'
+
+      def self.call(client, data, match)
+        client.say(channel: data.channel, text: "#{match[:command]}: #{match[:expression]}")
+      end
+    end
+  end
+
+  let! :command2 do
+    Class.new(SlackRubyBot::Commands::Base) do
+      command 'command'
+
+      def self.call(client, data, match)
+        client.say(channel: data.channel, text: "#{match[:command]}: #{match[:expression]}")
+      end
+    end
+  end
+
+  def app
+    SlackRubyBot::App.new
+  end
+
+  describe '#command_classes' do
+    it 'returns all registered command classes' do
+      expect(SlackRubyBot::Commands::Base.command_classes).to include command1
+      expect(SlackRubyBot::Commands::Base.command_classes).to include command2
+    end
+
+    it 'returns command1 before command2' do
+      command_classes = SlackRubyBot::Commands::Base.command_classes
+      expect(command_classes.find_index(command1)).to be < command_classes.find_index(command2)
+    end
+  end
+end
