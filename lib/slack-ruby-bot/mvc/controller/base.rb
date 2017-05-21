f093c47ead612ad7d984fdfd33fc9fb295a0ba4c
Chuck Remes <git@chuckremes.com>
2017-05-21 17:53:42 -0500

    switch from alias_method to alias to quiet rubocop

--- lib/slack-ruby-bot/mvc/controller/base.rb
+++ lib/slack-ruby-bot/mvc/controller/base.rb
@@ -6,7 +6,7 @@ module SlackRubyBot
 
         class << self
           attr_reader :abstract
-          alias_method :abstract?, :abstract
+          alias :abstract? :abstract
 
           def controllers
             Base.instance_variable_get(:@controllers)
