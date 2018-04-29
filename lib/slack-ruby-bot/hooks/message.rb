a7b57bb9dd63bdc6017eb2d554ef47a25f420ecf
naosuke <info@naosuke.me>
2018-04-29 23:44:54 +0900

    [lint] Thawing String of data.text

--- lib/slack-ruby-bot/hooks/message.rb
+++ lib/slack-ruby-bot/hooks/message.rb
@@ -2,6 +2,7 @@ module SlackRubyBot
   module Hooks
     class Message
       def call(client, data)
+        data.text = +data.text if data.text   # Thawing data.text
         return if message_to_self_not_allowed? && message_to_self?(client, data)
         data.text.strip! if data.text
         result = child_command_classes.detect { |d| d.invoke(client, data) }
