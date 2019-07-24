5d7a3b1aa57d9530dfe623950dc4a5a625867e32
dblock <dblock@dblock.org>
2019-07-24 08:01:40 -0400

    Do not log connection on reconnect.

--- lib/slack-ruby-bot/hooks/hello.rb
+++ lib/slack-ruby-bot/hooks/hello.rb
@@ -2,6 +2,7 @@ module SlackRubyBot
   module Hooks
     class Hello
       attr_accessor :logger
+      attr_accessor :connected_at
 
       def initialize(logger)
         self.logger = logger
@@ -9,7 +10,9 @@ module SlackRubyBot
 
       def call(client, _data)
         return unless client && client.team
+        return if connected_at
         logger.info "Successfully connected team #{client.team.name} (#{client.team.id}) to https://#{client.team.domain}.slack.com."
+        @connected_at = Time.now.utc
       end
     end
   end
