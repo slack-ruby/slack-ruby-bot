58edabef8a88ee1cf3afd52427fc5a0780dddee7
Nicholas Koh <nkohwl@gmail.com>
2020-03-22 19:46:32 +0800

    test: Add spec for logging the connection

--- spec/slack-ruby-bot/hooks/hello_spec.rb
+++ spec/slack-ruby-bot/hooks/hello_spec.rb
@@ -1,19 +1,22 @@
 describe SlackRubyBot::Hooks::Hello do
   let(:logger) { double(:logger, info: nil) }
-  let(:message_hook) { described_class.new(logger) }
+  let(:hello_hook) { described_class.new(logger) }
 
   describe '#initialize' do
     it 'sets reconnected' do
-      expect(message_hook.instance_variable_get(:@reconnected)).to eq(false)
+      expect(hello_hook.instance_variable_get(:@reconnected)).to eq(false)
     end
   end
 
-  # describe '#call' do
-  #   it 'doesn\'t raise an error when message is a frozen string' do
-  #     message_hook.call(
-  #       SlackRubyBot::Client.new,
-  #       Hashie::Mash.new(text: 'message'.freeze)
-  #     )
-  #   end
-  # end
+  describe '#call' do
+    let(:team_name) { 'Example Team' }
+    let(:team_id) { SecureRandom.uuid }
+    let(:team_domain) { 'example' }
+    let(:team) { double(:team, name: team_name, id: team_id, domain: team_domain) }
+    let(:client) { instance_double(SlackRubyBot::Client, team: team ) }
+    it 'logs the connection' do
+      expect(logger).to receive(:info).with("Successfully connected team #{team_name} (#{team_id}) to https://#{team_domain}.slack.com.")
+      hello_hook.call(client, double)
+    end
+  end
 end
