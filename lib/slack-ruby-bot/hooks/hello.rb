0d9a2dd262871bf6a712db7597e8d5d2138059e6
Nicholas Koh <nkohwl@gmail.com>
2020-03-23 21:54:15 +0800

    refactor: Record the connected_at time instead of boolean

--- lib/slack-ruby-bot/hooks/hello.rb
+++ lib/slack-ruby-bot/hooks/hello.rb
@@ -1,17 +1,16 @@
 module SlackRubyBot
   module Hooks
     class Hello
-      attr_accessor :logger
+      attr_accessor :logger, :connected_at
 
       def initialize(logger)
         self.logger = logger
-        @reconnected = false
       end
 
       def call(client, _data)
         return unless client && client.team
-        logger.info "Successfully #{@reconnected ? 'reconnected' : 'connected'} team #{client.team.name} (#{client.team.id}) to https://#{client.team.domain}.slack.com."
-        @reconnected = true
+        logger.info "Successfully #{@connected_at ? 'reconnected' : 'connected'} team #{client.team.name} (#{client.team.id}) to https://#{client.team.domain}.slack.com."
+        @connected_at = Time.now.utc
       end
     end
   end
