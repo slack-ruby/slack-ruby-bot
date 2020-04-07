819ccdc0ede5dfd7f51276e674dfb7d7fe1eeb27
Nicholas Koh <nkohwl@gmail.com>
2020-04-07 20:25:34 +0800

    test: Move specs into shared example

--- lib/slack-ruby-bot/rspec/support/slack-ruby-bot/it_behaves_like_a_slack_bot.rb
+++ lib/slack-ruby-bot/rspec/support/slack-ruby-bot/it_behaves_like_a_slack_bot.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 shared_examples 'a slack ruby bot' do
+  let(:token) { 'slack-api-token' }
+
   context 'not configured' do
     before do
       @slack_api_token = ENV.delete('SLACK_API_TOKEN')
@@ -11,6 +13,47 @@ shared_examples 'a slack ruby bot' do
     end
     it 'requires SLACK_API_TOKEN' do
       expect { subject }.to raise_error RuntimeError, "Missing ENV['SLACK_API_TOKEN']."
+    end
+  end
+
+  context 'when SLACK_API_TOKEN is defined in ENV but not config' do
+    it 'sets config.token from ENV' do
+      allow(ENV).to receive(:[]).and_call_original.at_least(:once)
+      allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(token)
+      SlackRubyBot.configure { |config| config.token = nil }
+
+      expect(app.config.token).to eq(token)
+    end
+  end
+
+  context 'when SLACK_API_TOKEN is not defined in ENV but is defined in config' do
+    it 'sets config.token from config' do
+      allow(ENV).to receive(:[]).at_least(:once)
+      allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(nil)
+      SlackRubyBot.configure { |config| config.token = token }
+
+      expect(app.config.token).to eq(token)
+    end
+  end
+
+  context 'when SLACK_API_TOKEN is defined in ENV and config' do
+    it 'sets config.token from config' do
+      token2 = 'another-api-token'
+      allow(ENV).to receive(:[]).at_least(:once)
+      allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(token2)
+      SlackRubyBot.configure { |config| config.token = token }
+
+      expect(app.config.token).to eq(token)
+    end
+  end
+
+  context 'when SLACK_API_TOKEN is not defined in ENV or config' do
+    it 'raises error' do
+      allow(ENV).to receive(:[]).at_least(:once)
+      allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(nil)
+      SlackRubyBot.configure { |config| config.token = nil }
+
+      expect { app }.to raise_error("Missing ENV['SLACK_API_TOKEN'].")
     end
   end
 end
--- spec/slack-ruby-bot/app_spec.rb
+++ spec/slack-ruby-bot/app_spec.rb
@@ -6,51 +6,6 @@ describe SlackRubyBot::App do
   end
   it_behaves_like 'a slack ruby bot'
 
-  describe '.initialize' do
-    let(:token) { 'slack-api-token' }
-
-    context 'when SLACK_API_TOKEN is defined in ENV but not config' do
-      it 'sets config.token from ENV' do
-        allow(ENV).to receive(:[]).and_call_original.at_least(:once)
-        allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(token)
-        SlackRubyBot.configure { |config| config.token = nil }
-
-        expect(app.config.token).to eq(token)
-      end
-    end
-
-    context 'when SLACK_API_TOKEN is not defined in ENV but is defined in config' do
-      it 'sets config.token from config' do
-        allow(ENV).to receive(:[]).at_least(:once)
-        allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(nil)
-        SlackRubyBot.configure { |config| config.token = token }
-
-        expect(app.config.token).to eq(token)
-      end
-    end
-
-    context 'when SLACK_API_TOKEN is defined in ENV and config' do
-      it 'sets config.token from config' do
-        token2 = 'another-api-token'
-        allow(ENV).to receive(:[]).at_least(:once)
-        allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(token2)
-        SlackRubyBot.configure { |config| config.token = token }
-
-        expect(app.config.token).to eq(token)
-      end
-    end
-
-    context 'when SLACK_API_TOKEN is not defined in ENV or config' do
-      it 'raises error' do
-        allow(ENV).to receive(:[]).at_least(:once)
-        allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(nil)
-        SlackRubyBot.configure { |config| config.token = nil }
-
-        expect { app }.to raise_error("Missing ENV['SLACK_API_TOKEN'].")
-      end
-    end
-  end
-
   describe '.instance' do
     it 'creates an instance of the App subclass' do
       klass = Class.new(SlackRubyBot::App)
