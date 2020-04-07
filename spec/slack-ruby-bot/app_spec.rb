e9a4f68dee2cdcba12296931106ca203d0efbb13
Nicholas Koh <nkohwl@gmail.com>
2020-04-07 20:10:39 +0800

    test: Add spec for current config.token behaviour

--- spec/slack-ruby-bot/app_spec.rb
+++ spec/slack-ruby-bot/app_spec.rb
@@ -6,6 +6,26 @@ describe SlackRubyBot::App do
   end
   it_behaves_like 'a slack ruby bot'
 
+  describe '.initialize' do
+    let(:token) { 'slack-api-token' }
+
+    it 'sets config.token' do
+      allow(ENV).to receive(:[]).and_call_original.at_least(:once)
+      allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(token)
+
+      expect(app.config.token).to eq(token)
+    end
+
+    context 'when SLACK_API_TOKEN is not defined in ENV' do
+      it 'raises error' do
+        allow(ENV).to receive(:[]).at_least(:once)
+        allow(ENV).to receive(:[]).with('SLACK_API_TOKEN').and_return(nil)
+
+        expect { app }.to raise_error("Missing ENV['SLACK_API_TOKEN'].")
+      end
+    end
+  end
+
   describe '.instance' do
     it 'creates an instance of the App subclass' do
       klass = Class.new(SlackRubyBot::App)
